7954b21c6db29d21327489aca229ce1ab59ef76f
hamzaEnigma <hamzasomai94@gmail.com>
2024-04-20 16:24:09 +0200

    commit(feat): add appointement entity

---
+++ LPR/LPR.DAL/Entities/Appointement.cs
@@ -0,0 +1,16 @@
+﻿using LPR.DAL.Entities.CommonEntities;
+
+namespace LPR.DAL.Entities
+{
+    public class Appointement : EntityBase
+    {
+        public Guid Id { get; set; }
+        public required Guid ProfesionnalId { get; set; }
+        public virtual Professional? Professional { get; set; }
+        public required string DateAppointement { get; set; }
+        public required string HourAppointement { get; set; }
+        public string? Motif { get; set; }
+        public string? Comment { get; set; }
+
+    }
+}
