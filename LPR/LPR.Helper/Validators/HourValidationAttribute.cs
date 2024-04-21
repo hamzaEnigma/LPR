e7a61e18e22958730338801c2222af4db3e373b7
hamzaEnigma <hamzasomai94@gmail.com>
2024-04-21 08:16:49 +0200

    commit(feat): hour validation add

---
+++ LPR/LPR.Helper/Validators/HourValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using LPR.Helper.ConfigData;
+using LPR.Helper.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LPR.Helper.Validators
+{
+    public class HourValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string? strValue = value as string;
+            HourConfig hours = new HourConfig();
+            if (!hours.hoursOfDay.Contains(strValue))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
